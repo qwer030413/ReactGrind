4d9d722e812cdbb2961f82bb5f0e86651da7aae9
Chris <comosjay@gmail.com>
2024-01-06 17:44:25 -0800

    aaaa

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -22,7 +22,7 @@ function App() {
       case "/pages/aboutme.js":
         component = <Aboutme />
         break;
-      case "/ReactGrind":
+      case "/ReactGrind/":
         component = <Home />
         break;
 
