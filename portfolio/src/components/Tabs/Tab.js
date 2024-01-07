da8904fc8bb145b249d9f68ee8697be587531706
Chris <comosjay@gmail.com>
2024-01-06 18:54:26 -0800

    aaaa

--- portfolio/src/components/Tabs/Tab.js
+++ portfolio/src/components/Tabs/Tab.js
@@ -19,7 +19,9 @@ const Tabs = () => {
           <div className="tabcontainer"> 
             
               
-               <Bouncebtn text = "About Me"  h = "../../pages/aboutme.js" />
+               {/* <Bouncebtn text = "About Me"  h = "../../pages/aboutme.js" /> */}
+               <Bouncebtn text = "About Me"  h = "https://qwer030413.github.io/pages/aboutme.js" />
+
                <Bouncebtn text = "Projects" h = "../../pages/Projects.js" />
                <Bouncebtn text = "Contact me" h = "../../pages/contactme.js" />
               
